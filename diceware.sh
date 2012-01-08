6d891268cecea5ae48a677ecfc234dbcfa31652f
Louis Wilson <louis.wilson@asu.edu>
2012-01-07 17:22:09 -0700

    Added download option and refactored diceware.sh

--- diceware.sh
+++ diceware.sh
@@ -1,29 +1,53 @@
 #!/bin/sh
-WORDLIST=$HOME/code/beale.wordlist
+wordlist="$HOME/code/beale.wordlist.asc"
+nrolls=5
+downloadloc=http://world.std.com/~reinhold/beale.wordlist.asc
 
 isint() {
-	[ "$1" -eq "$1" 2>/dev/null ];
+	[ "$1" -eq "$1" ] 2>/dev/null;
 }
 
-if [ $# -ge 1 ] && isint "$1"; then
-	nrolls=$1;
-else
-	nrolls=5;
-fi
-
-if [ $# -ge 2 -a -f "$2" ]; then
-	WORDLIST=$2
-fi
+while (( $# )); do
+	case "$1" in
+		-n|--nrolls)
+			nrolls=$2
+			shift
+			;;
+		-w|--wordlist)
+			wordlist=$2
+			shift
+			;;
+		--download)
+			if command -v curl >/dev/null; then
+				curl -s -O "$downloadloc"
+				exit 0
+			elif command -v wget >/dev/null; then
+				wget -q "$downloadloc"
+				exit 0
+			else
+				echo "No wget or curl available; could not download." >&2
+			fi
+			;;
+		*)
+			if isint "$1"; then
+				nrolls=$1
+			else
+				echo "Unknown option $1" >&2
+			fi
+			;;
+	esac
+	shift
+done
 
 for (( i=0; i<$nrolls; ++i )); do
 	# The sequence of rolls for this word
-	rseq=;
+	rseq=
 	for j in {1..5}; do
 		# `od -N1 -An -i /dev/urandom` echos a random byte in decimal
 		# notation (to the extent that /dev/urandom is random)
-		let "roll=$(od -N1 -An -i /dev/urandom)*6 / 256 + 1";
-		rseq="$rseq$roll";
-	done;
+		let "roll=$(od -N1 -An -i /dev/urandom) * 6 / 256 + 1"
+		rseq=$rseq$roll
+	done
 	# print out the word on the line for $rseq
-	grep "^$rseq" $WORDLIST | cut -f2
+	grep "^$rseq" "$wordlist" | cut -f2
 done
